2808044f0ad3eb81d42d2077aef75db87befc9d8
Vladimir <58466406+Vladimir-od@users.noreply.github.com>
2021-10-19 10:10:47 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 "use strict";
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -11,3 +13,6 @@ schemeSvg.addEventListener("click", (event) => {
     totalPriceTag.textContent = totalPrice;
   }
 });
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('is-open');
+})
